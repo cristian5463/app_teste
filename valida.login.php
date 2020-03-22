bfc8297e39e6cea6ac29ced3802c7bd89b19962f
cristian5463 <cristian.ctrevi@gmail.com>
2020-03-21 22:51:59 -0300

    arquivos do app

---
+++ valida.login.php
@@ -0,0 +1,17 @@
+<?php
+
+    //váriavel para verificar os usuários do sistema estão cadastrados
+    $verifica_validacao = false;
+
+    //usuarios cadastrados no sistema (utilização de hard code, não recomendado utilização, somente aplicado nesse código pois este não tem banco de dados);
+    $usuarios = array(
+        array('email' => 'teste@teste.com.br', 'senha' => '12345');
+        array('email' => 'usuario@teste.com.br', 'senha' => 'abcde');
+    );
+
+    foreach($usuarios as $user) {
+        
+    }
+
+
+?>
